4b095db48236835f2343d9e7b063302a672a5825
Ahastan <ahastan.surees23@ycdsbk12.ca>
2022-01-19 13:35:34 -0500

    formatted introduction page

--- Sketch1.java
+++ Sketch1.java
@@ -2,10 +2,10 @@ import processing.core.PApplet;
 import processing.core.PFont;
 
 public class Sketch1 extends PApplet {
-  PFont f;
+  PFont text;
   PFont title;
   PFont subtitle;
-  public boolean contnue = false;
+  public boolean introductionMove = false;
   public String [] teams = {"Chicago Bulls", "Miami Heat", "Brooklyn Nets", "Cleveland Cavaliers", 
     "Milwaukee Bucks", "Philadelphia 76ers", "Charlotte Hornets", "Washington Wizards", "Toronto Raptors", "Boston Celtics", 
     "New York Knicks", "Atlanta Hawks", "Indiana Pacers", "Detroit Pistons", "Orlando Magic", "Phoenix Suns", "Golden State Warriors", "Memphis Grizzlies", "Utah Jazz", "Dallas Mavericks", 
@@ -25,7 +25,7 @@ public class Sketch1 extends PApplet {
     background(92, 150, 242);
     subtitle = createFont("DialogInput.italic", 16, true);
     title = createFont("DialogInput.bolditalic", 75);
-    f = createFont("Playbill", 75);
+    text = createFont("Playbill", 75);
   }
 
   /**
@@ -34,13 +34,40 @@ public class Sketch1 extends PApplet {
   public void draw() {
 
     // Introduction Screen
+    screen1();
+    
+  }
+  
+
+  public void mouseClicked()
+  {
+    if(mouseX > 20 && mouseX < 460 && mouseY > 175 && mouseY < 460)
+    {
+      int teamArray = 0;
+      for (int h = 20; h < 460; h = h + 92)
+      {
+        for (int v = 175; v < 460; v = v + 50)
+        {
+          if (mouseX >= h && mouseX <= (h+92) && mouseY >= v && mouseY <= (v+50))
+          {
+            playerTeam = teams[teamArray];
+            introductionMove = true;
+          }
+          teamArray++;
+        }
+      }
+    }
+  }
+
+  public void screen1()
+  {
     textFont(subtitle, 20);
     fill(0);
     text("Welcome to", 200, 30);
     textFont(title, 27);
     fill(0);
     text("NBA ULTIMATE TEAM SIMULATOR", 40, 60);
-    textFont(f, 35);
+    textFont(text, 35);
     fill(18, 109, 128);
     text("CHOOSE YOUR FAVORITE TEAM", 110, 150);
 
@@ -56,43 +83,18 @@ public class Sketch1 extends PApplet {
         rect(i,j,92,50);
         
         
-        textFont(f, 22);
+        textFont(text, 22);
         fill(18, 109, 128);
         text(teams[count], i, j, 92, 225);
 
         count++;
       }
     }
-  
-    // stroke(225);
-    // fill(225);
-    // rect(50,100, 70, 70);
 
-    if (contnue == true)
+    if (introductionMove == true)
     {
       background(92, 150, 242);
       text(playerTeam, 110, 150);
-    }
-  }
-  
-
-  public void mouseClicked()
-  {
-    if(mouseX > 20 && mouseX < 460 && mouseY > 175 && mouseY < 460)
-    {
-      int coun = 0;
-      for (int h = 20; h < 460; h = h + 92)
-      {
-        for (int v = 175; v < 460; v = v + 50)
-        {
-          if (mouseX >= h && mouseX <= (h+92) && mouseY >= v && mouseY <= (v+50))
-          {
-            playerTeam = teams[coun];
-            contnue = true;
-          }
-          coun++;
-        }
-      }
     }
   }
 
